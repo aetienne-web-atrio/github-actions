601c233443620d07d23ba926cbbc177513a7734f
aetienne-web-atrio <adrien.etienne@web-atrio.com>
2021-06-01 15:15:26 +0200

    Tests

--- main.py
+++ main.py
@@ -6,29 +6,18 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
     a = {"qssqd": "qsdsqd", "dqsdq": "qsdqsd", "qsdqsddqs": "qsdqsdqsd"}
 
     b = 6548
     f = 47698789
 
+    r = [{}, {}]
 
-    r = [
-        {
-
-        },
-        {
-
-
-
-        }
-    ]
-
-
-    print_hi('PyCharm')
+    print_hi("PyCharm")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
