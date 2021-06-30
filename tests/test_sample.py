fc4dd333651febfb85ed311a4e7f0257bb9fbcb5
Adrien ETIENNE <adrien.etienne@web-atrio.com>
2021-06-30 17:59:15 +0200

    blacked

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,16 @@
 # content of test_sample.py
-def inc(x):
-    return x + 1
+def inc(number):
+    """
+    Increment
+    :param number:
+    :return:
+    """
+    return number + 1
 
 
 def test_answer():
+    """
+    Test answer
+    :return:
+    """
     assert inc(3) == 4
