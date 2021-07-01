7f8c79b3b09763bd77249de7d8a0b88ff4cdff4f
Adrien ETIENNE <adrien.etienne@web-atrio.com>
2021-07-01 16:17:30 +0200

    Tests

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-
+    """
+    Hello world !
+    :return:
+    """
     return "<h1>Guy de lusignan</h1><p>Hello, World! HEY now here</p>"
